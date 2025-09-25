34e52b05b30f4c5fea520a6dcf51cfcc93b71ee8
Jackson Almeida <didi@Marias-MacBook-Pro.local>
2025-09-25 12:21:47 -0300

    Conexao com Supabase

--- core/settings.py
+++ core/settings.py
@@ -87,12 +87,13 @@ env = environ.Env()
 environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': env('DATABASE_HOST'),
         'PORT': env('DATABASE_PORT'),
+        'OPTIONS': {'sslmode': 'require'}, 
     }
 }
 
